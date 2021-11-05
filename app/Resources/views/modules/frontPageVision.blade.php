ebd093f2eb9dd835d27e2070ce1f42a5b7dd7afd
Nathan Merry <nathanmerry9713@gmail.com>
2021-11-05 12:02:04 +0100

    npm run prod

--- app/Resources/views/modules/frontPageVision.blade.php
+++ app/Resources/views/modules/frontPageVision.blade.php
@@ -1,4 +1,3 @@
-
 <section class="py-10 lg:py-20 frontPageVision">
   <div class="relative">
     <div class="relative z-30 px-2 mx-auto max-w-container-sm lg:px-0">
@@ -9,8 +8,10 @@
         <img src="{{ $module['image'] }}" alt="{{ $module['title_small'] }}" class="z-10 w-1/2 mx-auto">
       @endif
     </div>
-    <div class="absolute bottom-0 transform translate-x-1/2 right-1/2">
-      @include('svgs.dots', ['class' => 'w-64'])
-    </div>
+    @if ($module['image_secondary'])
+      <div class="absolute bottom-0 transform translate-x-1/2 right-1/2">
+        <img src="{{ $module['image_secondary'] }}" class="w-64">
+      </div>
+    @endif
   </div>
 </section>
